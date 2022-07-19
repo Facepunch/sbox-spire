e0ad9e2aeb55831920b8fc63aa043c2c9b2e9320
Tony <devultj@gmail.com>
2022-07-19 16:10:21 +0100

    Avatar separated, show score & placement borders

--- code/Gamemodes/Clash/UI/ClashHudPanel.cs
+++ code/Gamemodes/Clash/UI/ClashHudPanel.cs
@@ -1,5 +1,37 @@
 namespace Spire.Gamemodes.Clash;
 
+partial class ScoreAvatar : Panel
+{
+	public Image Avatar { get; set; }
+	public Label Label { get; set; }
+
+	public ScoreAvatar() : base()
+	{ 
+	}
+
+	public ScoreAvatar( Client cl, int score = 0, int position = 0 )
+	{
+		Avatar = AddChild<Image>( "avatar" );
+		Avatar.SetTexture( $"avatar:{cl.PlayerId}" );
+
+		Label = AddChild<Label>( "score" );
+
+		if ( score > 0 )
+		Label.Text = $"{score}";
+
+		SetClass( "dead", cl.Pawn is not PlayerCharacter );
+		
+		ApplyPosition( position );
+	}
+
+	void ApplyPosition( int position )
+	{
+		Avatar.SetClass( "gold", position == 0 );
+		Avatar.SetClass( "silver", position == 1 );
+		Avatar.SetClass( "bronze", position == 2 );
+	}
+}
+
 [UseTemplate]
 public partial class ClashHudPanel : Panel
 {
@@ -27,13 +59,20 @@ public partial class ClashHudPanel : Panel
 
 		TeamMembers.DeleteChildren( true );
 		LastUpdatedAvatars = 0;
+		
+		var game = BaseGamemode.Current as ClashGamemode;
+		var sortedScores = game.Scores.OrderByDescending( kvp => kvp.Value ).ToList();
 
-		foreach ( var client in Client.All )
+		int position = 0;
+		foreach ( var kvp in sortedScores )
 		{
-			var panel = TeamMembers.AddChild<Image>( "avatar" );
+			var client = kvp.Key;
+			var score = kvp.Value;
+			var avatar = new ScoreAvatar( client, score, position );
 
-			panel.SetTexture( $"avatar:{client.PlayerId}" );
-			panel.SetClass( "dead", client.Pawn is not PlayerCharacter );
+			TeamMembers.AddChild( avatar );
+
+			position++;
 		}
 	}
 }
